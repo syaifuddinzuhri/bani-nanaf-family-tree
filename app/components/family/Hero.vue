8daf4560ca9c4870901c9af6a6681e28aeadf1bb
Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2026-04-06 16:50:30 +0700

    fix

--- app/components/family/Hero.vue
+++ app/components/family/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center space-y-4 pt-16 ">
+  <div class="flex flex-col items-center space-y-4 pt-8">
     <div
       class="relative w-40 h-40 flex items-center justify-center transition-transform duration-300 hover:scale-110"
     >
