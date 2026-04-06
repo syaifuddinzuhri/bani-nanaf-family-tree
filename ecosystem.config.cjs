8b6765aaa9772f3b70f78f191c3138cbd2405d34
Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2026-04-06 14:53:27 +0700

    first commit

---
+++ ecosystem.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "family-tree",
+      port: "5000",
+      exec_mode: "fork", // Karena VPS 1 Core, pakai mode fork saja
+      instances: "1",
+      script: "./output/server/index.mjs",
+      env: {
+        NODE_ENV: "production",
+        NITRO_PORT: "5000", // Port Nuxt (Nitro)
+        NITRO_HOST: "127.0.0.1"
+      },
+      max_memory_restart: "500M", // Restart otomatis jika memakan RAM > 500MB
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm Z"
+    }
+  ]
+};
